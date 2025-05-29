3dab7c1ad1aafbc6c471ff9df63b95b0d88f0485
blenderfan <blenderfan@gmx.at>
2025-05-29 15:08:36 +0200

    Messaging from scratch

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,7 +73,7 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.12.0") // Ensure base OkHttp is also present
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0") // Or latest versions
     implementation ("androidx.activity:activity-compose:1.9.0")
-    implementation ("androidx.compose.material3:material3:1.3.2")
+    implementation ("androidx.compose.material3:material3:1n.3.2")
     implementation ("androidx.navigation:navigation-compose:2.7.7" )
     implementation ("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0" )
     implementation ("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0" )
